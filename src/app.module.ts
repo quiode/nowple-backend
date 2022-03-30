3f36fbb141a9af2c316d020870c0546cd0f610fd
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-03-30 16:41:06 +0200

    basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SettingsModule } from './settings/settings.module';
+import { PreferencesModule } from './preferences/preferences.module';
+import { MessagesModule } from './messages/messages.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, SettingsModule, PreferencesModule, MessagesModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
