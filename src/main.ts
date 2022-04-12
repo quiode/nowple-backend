4a8002dc422a013fda30d73b4c4dfa4ebf374d77
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-04-12 21:11:53 +0200

    dev

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,14 @@ import * as fs from 'fs';
 import * as spdy from 'spdy';
 import * as express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { isProduction } from './shared/constants';
 
 async function bootstrap() {
   const expressApp: Express = express();
 
   const spdyOpts: ServerOptions = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./chain.pem'),
+    key: isProduction ? fs.readFileSync('./key.pem') : fs.readFileSync('./test.key'),
+    cert: isProduction ? fs.readFileSync('./chain.pem') : fs.readFileSync('./test.crt'),
     spdy: {
       protocols: ['h2', 'http/1.1', 'http/1.0'],
     },
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,5 @@
 export const jwtConstants = {
   secret: 'secretKey',
 };
+
+export const isProduction = false;
