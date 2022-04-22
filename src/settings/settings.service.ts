854c7235f168f6b6255e6dbfd459efeac9fca221
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-04-22 14:02:58 +0200

    considerPolitics, considerGender, reversedPoliticalView, preferredGender, gender

--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -37,6 +37,16 @@ export class SettingsService {
     const settings = user.settings;
     settings.isDarkMode = body.isDarkMode != null ? body.isDarkMode : settings.isDarkMode;
     settings.discoverable = body.discoverable != null ? body.discoverable : settings.discoverable;
+    settings.considerGender =
+      body.considerGender != null ? body.considerGender : settings.considerGender;
+    settings.considerPolitics =
+      body.considerPolitics != null ? body.considerPolitics : settings.considerPolitics;
+    settings.reversedPoliticalView =
+      body.reversedPoliticalView != null
+        ? body.reversedPoliticalView
+        : settings.reversedPoliticalView;
+    settings.preferredGender =
+      body.preferredGender != null ? body.preferredGender : settings.preferredGender;
 
     await this.settingsRepository.save(settings);
 
