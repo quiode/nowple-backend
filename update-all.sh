4a29f642495de3a02e92c58eafa4c88f64616876
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-05-09 10:26:33 +0200

    update dependencies

--- update-all.sh
+++ update-all.sh
@@ -2,12 +2,14 @@
 cd ../nowple-website/ || exit
 sudo rm -r dist
 git pull
+npm i --production
 ng build
 sudo rm -r ../nowple-backend/client/*
 sudo cp -r dist/nowple-website/* ../nowple-backend/client/
 cd ../nowple-backend/ || exit
 sudo systemctl stop nowple.service
 git pull
+npm i --production
 sudo rm -r dist
 npm run build
 typeorm schema:sync
